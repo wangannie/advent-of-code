1c9e6e51abff8e4ee5912dd08d04084bd2e47b70
Annie Wang <wangannie@berkeley.edu>
2020-12-10 14:08:59 +0000

    Finish day 10

---
+++ 10.py
@@ -0,0 +1,32 @@
+def count_diffs(nums):
+    nums.sort()
+    prev = 0
+    ones, threes = 0, 0
+    for i in range(len(nums)):
+        diff = nums[i] - prev
+        if diff == 1:
+            ones += 1
+        elif diff == 3:
+            threes += 1
+        prev = nums[i]
+    return (ones, threes + 1)
+
+
+def part1(data):
+    nums = [int(n) for n in data.splitlines()]
+    ones, threes = count_diffs(nums)
+    return ones * threes
+
+
+def part2(data):
+    nums = [int(n) for n in data.splitlines()]
+    nums.sort()
+    target = nums[-1] + 3
+    nums.append(target)
+    dp = {}
+    dp[0] = 1
+    dp[nums[0]] = 1
+    for i in range(1, len(nums)):
+        dp[nums[i]] = dp.get(nums[i] - 1, 0) + dp.get(nums[i] - 2, 0) + dp.get(
+            nums[i] - 3, 0)
+    return dp[target]
